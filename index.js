be49b18b62b73c5b3174d7800e21b4330f5140b2
tenmakamatani <tenmakamatani0@gmail.com>
2019-08-13 22:50:40 +0900

    [update]コマンドライン引数からハッシュ化できるように

--- index.js
+++ index.js
@@ -3,7 +3,15 @@
 const crypto = require("crypto");
 
 const shasum = crypto.createHash("sha1");
-shasum.update("赤巻紙青巻紙気巻紙");
+
+const sentence = process.argv[2];
+if (sentence == undefined) {
+    console.log("\n---- How to use ----\n");
+    console.log("node index.js something\n");
+    process.exit(1);
+}
+
+shasum.update(sentence);
 let hash = shasum.digest("hex");
 
 console.log(hash);
